472622e0c452f38b283e14925c28242fbd214d0f
otutay <otutaysalgir@gmail.com>
2021-03-24 00:24:32 +0300

    continues regOp

--- srcRtl/vhdl/alu.vhd
+++ srcRtl/vhdl/alu.vhd
@@ -6,7 +6,7 @@
 -- Author     : osmant  <otutaysalgir@gmail.com>
 -- Company    :
 -- Created    : 2021-03-22
--- Last update: 2021-03-23
+-- Last update: 2021-03-24
 -- Platform   :
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -42,11 +42,18 @@ entity alu is
 end entity alu;
 
 architecture rtl of alu is
-  signal memOut : tMemOp := cMemOp;
+  signal memOut     : tMemOp                                   := cMemOp;
+  signal memOuti1   : tMemOp                                   := cMemOp;
+  signal operand1   : std_logic_vector(cXLen-1 downto 0)       := (others => '0');
+  signal operand2   : std_logic_vector(cXLen-1 downto 0)       := (others => '0');
+  signal operation  : tArithEnum                               := eNOOP;
+  signal regAddr    : std_logic_vector(cRegSelBitW-1 downto 0) := (others => '0');
+  signal regOpValid : std_logic                                := '0';
+  signal regOut     : tRegOp                                   := cRegOp;
 begin  -- architecture rtl
 
   -- load store op
-
+  oMemWB <= memOuti1;
   loadStorePro : process (iClk) is
   begin  -- process loadStorePro
     if iClk'event and iClk = '1' then   -- rising clock edge
@@ -65,6 +72,67 @@ begin  -- architecture rtl
       end if;
     end if;
   end process loadStorePro;
+
+  loadStoreRegPRo : process (iClk) is
+  begin  -- process loadStoreRegPRo
+    if iClk'event and iClk = '1' then   -- rising clock edge
+      memOuti1 <= memOut;
+    end if;
+  end process loadStoreRegPRo;
+
+  ----   register opeartion
+
+  regOpSelPro : process (iClk) is
+  begin  -- process regOpSelPro
+    if iClk'event and iClk = '1' then   -- rising clock edge
+      if(iDecodedReg.opRs1 = '1') then
+        operand1 <= iDecoded.rs1Data;
+      elsif(iDecodedReg.opPc = '1') then
+        operand1 <= iDecoded.curPc;
+      elsif(iDecodedReg.opImm = '1') then
+        operand1 <= iDecoded.imm;
+      end if;
+
+      if(iDecodedReg.opRs2 = '1') then
+        operand2 <= iDecoded.rs2Data;
+      elsif(iDecodedReg.opConst = '1') then
+        operand2 <= std_logic_vector(to_unsigned(4, cXLen));
+      elsif(iDecodedReg.opImm = '1') then
+        operand2 <= iDecoded.imm;
+      elsif(iDecodedReg.opPc = '1') then
+        operand2 <= iDecoded.curPc;
+      end if;
+
+    end if;
+  end process regOpSelPro;
+
+  regOpRegPro : process (iClk) is
+  begin  -- process regOpRegPro
+    if iClk'event and iClk = '1' then   -- rising clock edge
+      operation  <= iDecodedReg.arithType;
+      regAddr    <= iDecoded.rdAddr;
+      regOpValid <= iDecodedReg.dv;
+    end if;
+  end process regOpRegPro;
+
+  regOpDonePro : process (iClk) is
+  begin  -- process regOpDonePro
+    if iClk'event and iClk = '1' then   -- rising clock edge
+      regOut.addr <= regAddr;
+      regOut.dv   <= regOpValid;
+      case operation is
+        when eAdd =>
+          regOut.data <= signed(operand1) + signed(operand2);
+        when eSub =>
+          regOut.data <= signed(operand1) - signed(operand2);
+        when eShftLeft =>
+          regOut.data <= operand1 sll to_integer(unsigned(operand2(cRegSelBitW-1 downto 0)));
+        when eCompareSigned =>
+          regOut.data <=
+        when others => null;
+      end case;
+    end if;
+  end process regOpDonePro;
 
 
 end architecture rtl;
