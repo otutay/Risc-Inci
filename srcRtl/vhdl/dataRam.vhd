f5f85f80e8c0112855f3120e6faa2f71fc30bd7c
otutay <otutaysalgir@gmail.com>
2021-07-06 06:35:44 +0300

    data ram

--- srcRtl/vhdl/dataRam.vhd
+++ srcRtl/vhdl/dataRam.vhd
@@ -34,10 +34,25 @@ entity dataRam is
 
 end entity dataRam;
 architecture rtl of dataRam is
-  signal
+  signal ramEn      : std_logic                          := '0';
+  signal ramWEn     : std_logic                          := '0';
+  signal ramAddr    : std_logic_vector(cXLen-1 downto 0) := (others => '0');
+  signal ramDataIn  : std_logic_vector(cXLen-1 downto 0) := (others => '0');
+  signal ramDataOut : std_logic_vector(cXLen-1 downto 0) := (others => '0');
 
 
 begin  -- architecture rtl
+
+  ramCtrlPro : process (all) is
+  begin  -- process ramCtrlPro
+    ramEn     <= iMemOp.readDv or iMemOp.writeDv;
+    ramWEn    <= iMemOp.writeDv;
+    ramAddr   <= iMemOp.addr;
+    ramDataIn <= iMemOp.data;
+  end process ramCtrlPro;
+
+
+
   InstRam : entity work.ram
     generic map (
       cRamPerformance => "HIGH_PERFORMANCE",
@@ -47,11 +62,11 @@ begin  -- architecture rtl
     port map (
       iClk   => iClk,
       iRstA  => iRst,
-      iEnA   => '1',
-      iWEnA  => '0',
-      iAddrA => curPc(log2(cRamDepth-1)-1 downto 0),
-      iDataA => (others => '0'),
-      oDataA => instruction,
+      iEnA   => ramEn,
+      iWEnA  => ramWEn,
+      iAddrA => ramAddr(log2(cRamDepth-1)-1 downto 0),
+      iDataA => ramDataIn,
+      oDataA => ramDataOut,
       iRstB  => '0,
       iEnB   => '1'
       iWEnB  => instWen,
